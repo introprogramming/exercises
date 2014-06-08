0ac3d5185a5ede591c1d22904c9876e4c951279b
Leif Schelin <scleif@student.chalmers.se>
2014-06-08 14:33:17 +0200

    Added code and description for this simple exercise

---
+++ personnr/personnr.md
@@ -0,0 +1,9 @@
+
+Simple exercise: to check if a social security number is valid.
+
+Is run by  
+`python personnr.py [pnr]`
+
+1. Parses the input
+2. Checks that the date is valid
+3. Checks that the control number is valid
---
+++ personnr/personnr.py
@@ -0,0 +1,71 @@
+
+import sys
+import datetime
+
+########################
+## Functions
+
+def clean(string):
+    ix  = 0
+    list = []
+    for char in string:
+        if char != '-' and char != ' ':
+            list = list + [int(char)]
+    return list
+
+def dateCorrect(pnr):
+    string = ''
+    for i in pnr[2:6] :
+        string = string + str(i)
+    #print string
+    try:
+        datetime.datetime.strptime(string, '%m%d')
+    except ValueError:
+        return False
+    return True
+
+def numberCorrect(pnr):
+    factor = 2
+    c = 0
+    for x in pnr :
+        c = c + x*factor
+        if(x*factor > 9):
+            c = c -9
+        if factor == 2:
+            factor = 1
+        else :
+            factor = 2
+
+    #print c
+    return c%10==0
+
+def check(str):
+    cleaned = clean(pnr)
+    print cleaned
+
+    L = len(cleaned)
+    if L!=10:
+        return (False, 'Wrong number of elements')
+
+    if not dateCorrect(cleaned):
+        return (False, 'Error in date')
+
+    if not numberCorrect(cleaned):
+        return (False, 'Error in numer')
+
+    return (True, 'Congratulations, the number is correct')
+
+########################
+## Main
+
+pnr = "811218-9876"
+#print sys.argv
+if len(sys.argv)>1:
+    pnr = sys.argv[1]
+else :
+    print "Uses default number: ", pnr
+
+a = check(pnr)
+
+print a[1]
+
