47db6e4311b51544ac674bc8b9cf3f57a8c233d8
cpupro <practical_master@hotmail.com>
2014-06-13 19:30:44 +0200

    Added a palindrome exercise

---
+++ exercises/palindrome/README.md
@@ -0,0 +1,23 @@
+# Palindrome
+Gör ett program som kan bedömma om ett ord är en palindrom och som kan räkna antalet palindromer i en textfil.
+
+**Svårighetsgrad:** 2
+
+## Delmoment
+1. Skapa en funktion som givet en sträng bedömmer om strängen är en palindrom. **Svårighetsgrad 1.**
+2. Gör så att programmet kontinuerligt körs och givet input from terminalen skriver ut om input är en palindrom. **Svårighetsgrad 1.**
+3. Gör en funktion som rensar en sträng från onödiga tecken. **Svårighetsgrad 1.**
+4. Gör en funktion som givet en längre sträng räknar hur många palindromer som finns i strängen. **Svårighetsgrad 1.**
+5. Gör en funktion som läser en textfil och skriver ut hur många palindromer som finns i textfilen. **Svårighetsgrad 2.**
+6. Gör så att man väljer mellan läsa input- och läsa textfilläge när man startar programmet. **Svårighetsgrad 2.**
+
+```bash
+python palindrome.py
+Choose mode: (I)nteractive, (R)ead from file or (E)xit.
+: I
+Input string to check. (E)xit to exit.
+: Anna
+True
+: E
+Exiting
+```
---
+++ exercises/palindrome/palindrome.py
@@ -0,0 +1,89 @@
+#
+# Palindrome checker for English words.
+#
+# Usage:
+# python palindrome.py
+#
+
+import sys
+
+def is_palindrome(word):
+	"""Returns True if argument is a palindrome, returns False otherwise."""
+	word = word.lower().strip()
+	if word:    # Checks that word is not empty (empty sequences are false).
+		reverse_word = word[::-1]
+		return word == reverse_word
+	else:
+		return False
+	
+def clean_string(string):
+	"""Removes non-letters from string leaving only ascii letters (swedish characters removed),
+	whitespace and hyphens. Returns the clean string.
+	"""
+	new_string = ""
+	string.lower()
+	for c in string:
+		if c.isalpha() or c.isspace() or c == '-':
+			new_string += c
+	return new_string
+
+def count_palindromes(text):
+	"""Returns the number of palindromes in a text."""
+	count = 0
+	text = clean_string(text)
+	text = text.split()
+	for word in text:
+		if is_palindrome(word):
+			count += 1
+	return count
+
+def palindromes_in_file(path):
+	"""Prints the number of palindromes in file given by path."""
+	try:
+		file = open(path)
+		data = file.read()
+		print count_palindromes(data)
+		file.close()
+	except IOError as e:
+		print e.strerror
+
+def interactive_mode():
+	"""Repeatedly checks if given input from the command line are palindromes."""
+	print "Input string to check. (E)xit to exit."
+	while True:
+		input = raw_input(": ")
+		if input == 'E' or input == 'e':
+			print "Exiting"
+			exit(0)
+		else:
+			input = clean_string(input)
+			print is_palindrome(input)
+
+def file_mode():
+	"""Repeatedly counts the palindromes in file given from the command line."""
+	print "Input file name plus extension (file has to be in the same folder as python file)."
+	print "(E)xit to exit."
+	while True:
+		input = raw_input(": ")
+		if input == 'E' or input == 'e':
+			print "Exiting"
+			exit(0)
+		else:
+			palindromes_in_file(input)
+
+def main():
+	print "Choose mode: (I)nteractive, (R)ead from file or (E)xit."
+	while True:
+		mode = raw_input(": ")
+		if mode == 'I' or mode == 'i':
+			interactive_mode()
+		elif mode == 'R' or mode == 'r':
+			file_mode()
+		elif mode == 'E' or mode == 'e':
+			print "Exiting."
+			exit(0)
+		else:
+			print "Invalid input. (Valid inputs: I, R, E)"
+
+if __name__ == "__main__":
+	main()
