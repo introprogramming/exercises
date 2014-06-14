1e926883a921c98c5d0ddd8205985164afaba27b
Leif Schelin <scleif@student.chalmers.se>
2014-06-14 12:44:55 +0200

    Added new (simple) exercise for walking through a file tree

---
+++ exercises/printdir/README.md
@@ -0,0 +1,21 @@
+
+#Printdir
+
+Skriv ett program som skriver ut vad en mapp innehåller och hur mycket plats varje fil tar samt upprepa detta för mappens undermappar. Programmet ska läsa in vilken sökväg som ska undersökas.
+
+- Svårighetsgrad: 1
+
+##Moment
+
+1. Läs in en sökväg till en mapp (path). **Svårighetsgrad 1**
+2. Skriv ut vilka filer den innehåller och deras respektive storlek. **Svårighetsgrad 1**
+3. Upprepa för undermappar (subdirectories). **Svårighetsgrad 1**
+
+## Utbyggnad
+
+- Låt användaren lägga till ett `regex pattern` som filerna ska följa för att skrivas ut. **Svårighetsgrad 2**
+- Efter mappens namn: skriv ut summan av innehållet i mappen. **Svårighetsgrad 2**
+- Låt användaren utföra en operation på filerna som att kopiera eller byta namn på dem. *Obs! Testa inte detta på mappar med värdefullt innehåll. Testa alltid på kopior först!*  **Svårighetsgrad 2**
+
+## Externa bibliotek
+- (inga)
---
+++ exercises/printdir/printdir.py
@@ -0,0 +1,34 @@
+
+import sys
+import os
+from os.path import getsize, join
+
+def dir_contents_size(path):
+    """Prints how many bytes a directory contains.
+    
+    Shamelessly copied from https://docs.python.org/2/library/os.html#os.walk"""
+    for root, dirs, files in os.walk(path):
+        print root, "consumes",
+        print sum(getsize(join(root, name)) for name in files),
+        print "bytes in", len(files), "non-directory files"
+
+def dir_print_contents(path):
+    """Prints contents of a directory recursively.
+    
+    This implementation uses standard functions for iterating."""
+    for root, dirs, files in os.walk(path):
+        print ""
+        print root + os.sep
+        for file in files:
+            print "\t"+file+"\t", getsize(join(root, file)), "bytes"
+        
+def main():
+    """Prints contents of directory with specified path."""
+    path = "."
+    if len(sys.argv)>1:
+        path = sys.argv[1]
+    
+    print "In ", path, ":"
+    dir_print_contents(path)
+
+main()
