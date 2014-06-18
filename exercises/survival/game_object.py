4a7029b7eba0d25e376fb9c69e79aeccb23894a3
Leif Schelin <scleif@student.chalmers.se>
2014-06-18 13:39:03 +0200

    Camera is now fixed on the character. Represents world with a new class Board

--- exercises/survival/game_object.py
+++ exercises/survival/game_object.py
@@ -13,37 +13,104 @@ def vector_to(speed, from_x, from_y, target_x, target_y):
     
     return (x*speed/norm(x,y), y*speed/norm(x,y))
 
+class Board:
+    def __init__(self, win_width, win_height):
+        self.win_width = win_width
+        self.win_height = win_height
+        self.width = 1200
+        self.height = 1200
+        self.screen_x = (self.width - win_width)/2
+        self.screen_y = (self.height - win_height)/2
+    
+    def graph_position_of(self, board_x, board_y):
+        x = board_x - self.screen_x
+        y = board_y - self.screen_y
+        return (x, y)
+    
+    def set_screen_position(self, board_x, board_y):
+        self.screen_x = board_x - self.win_width/2
+        self.screen_y = board_y - self.win_height/2
+    
+    def board_position_of(self, graph_x, graph_y):
+        return (self.screen_x + graph_x, self.screen_y + graph_y)
+    
 class GraphObject(pygame.sprite.Sprite):
     board_width = 1200
     board_height = 1200
     
-    def __init__(self, image):
+    def __init__(self, image, board):
         pygame.sprite.Sprite.__init__(self)
         self.rect = self.image.get_rect()
-        self.vx = 200
-        self.vy = 200
-        
-    def set_position(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
-        
+        self.vx = 0
+        self.vy = 0
+        self.board_x = 0
+        self.board_y = 0
+        self.board = board
+    
+    def set_board_position(self, board_p):
+        (self.board_x, self.board_y) = board_p
+        self.update(0)
+    
+    #def set_screen_position(self, x, y):
+    #    self.rect.x = x
+    #    self.rect.y = y
+    
     def update(self, time):
-        self.rect.x = (time*self.vx + self.rect.x)%GraphObject.board_width
-        self.rect.y = (time*self.vy + self.rect.y)%GraphObject.board_height
+        self.board_x = (time*self.vx + self.board_x)%GraphObject.board_width
+        self.board_y = (time*self.vy + self.board_y)%GraphObject.board_height
+        (self.rect.x, self.rect.y) = self.board.graph_position_of(self.board_x, self.board_y)
 
 class Character(GraphObject):
     
     image = pygame.image.load(resource_path+"character.png")
+    speed = 100
     
-    def __init__(self):
-        GraphObject.__init__(self, Character.image.convert())
+    def __init__(self, board):
+        GraphObject.__init__(self, Character.image.convert(), board)
+        self.horizontal_dir = 0
+        self.vertical_dir = 0
         
+    
+    def stop(self, xdir, ydir):
+        if self.horizontal_dir == xdir:
+            self.horizontal_dir = 0
+        
+        if self.vertical_dir == ydir:
+            self.vertical_dir = 0
+        
+        self.fix_speed()
+    
+    def move(self, xdir, ydir):
+        if xdir != 0:
+            self.horizontal_dir = xdir
+        
+        if ydir  != 0:
+            self.vertical_dir = ydir
+        
+        self.fix_speed()
+        
+    def fix_speed(self):
+        if self.horizontal_dir == 0 and self.vertical_dir == 0:
+            self.vx = 0
+            self.vy = 0
+        else :
+            (self.vx, self.vy) = vector_to(Character.speed, 0, 0, self.horizontal_dir, self.vertical_dir)
+    
 class Projectile(GraphObject):
     image = pygame.image.load(resource_path+"projectile.png")
     speed = 500
+    max_time = 2
     
-    def __init__(self):
-        GraphObject.__init__(self, Character.image.convert())
+    def __init__(self, board):
+        GraphObject.__init__(self, Character.image.convert(), board)
+        self.time_travelled = 0
         
     def set_target(self, target_x, target_y):
         (self.vx, self.vy) = vector_to(Projectile.speed, self.rect.x, self.rect.y, target_x, target_y)
+        
+    def update(self, time):
+        GraphObject.update(self, time)
+        self.time_travelled = self.time_travelled + time
+        if self.time_travelled > Projectile.max_time:
+            #TODO
+            pass
--- exercises/survival/survival.py
+++ exercises/survival/survival.py
@@ -25,8 +25,9 @@ ticks = 0
 bypass_ticks = False
 
 # Game data
+board = game_object.Board(WIDTH, HEIGHT)
 sprites = pygame.sprite.Group()
-main_char = game_object.Character()
+main_char = game_object.Character(board)
 
 def draw():
     """Clears and draws objects to the screen"""
@@ -40,41 +41,62 @@ def draw():
 
 def shoot():
     global main_char
+    global board
     global sprites
     
-    p = game_object.Projectile()
-    p.set_position(main_char.rect.x, main_char.rect.y)
-    p.set_target(0,0)
+    p = game_object.Projectile(board)
+    p.set_board_position( (main_char.board_x, main_char.board_y) )
+    
+    (tx, ty) = pygame.mouse.get_pos()
+    p.set_target(tx, ty)
     sprites.add(p)
 
 def handle_input():
     """Handles the input"""
     
     # Global variables that might be changed
-    global bypass_ticks
     global running
-
+    global main_char
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                pass
+                main_char.move(-1, 0)
             elif event.key == pygame.K_RIGHT:
-                pass
+                main_char.move(1, 0)
             elif event.key == pygame.K_DOWN:
-                pass
+                main_char.move(0, 1)
             elif event.key == pygame.K_UP:
-                pass
+                main_char.move(0, -1)
             elif event.key == pygame.K_SPACE:
+                shoot()
+            elif event.key == pygame.K_ESCAPE:
+                running = False
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                main_char.stop(-1, 0)
+            elif event.key == pygame.K_RIGHT:
+                main_char.stop(1, 0)
+            elif event.key == pygame.K_DOWN:
+                main_char.stop(0, 1)
+            elif event.key == pygame.K_UP:
+                main_char.stop(0, -1)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
                 shoot()
     
 def update():
     """Updates the game logic"""
     global running
     global sprites
+    global board
+    global main_char
     
     time = 1.0 / UPDATES_PER_SEC
+    
+    board.set_screen_position(main_char.board_x, main_char.board_y)
     
     for s in sprites:
         s.update(time)
@@ -87,8 +109,13 @@ def main():
     
     global sprites
     global main_char
+    global board
+    
     sprites.add(main_char)
-    main_char.set_position(WIDTH/2, HEIGHT/2)
+    main_char.set_board_position( board.board_position_of( WIDTH/2, HEIGHT/2) )
+    other_char = game_object.Character(board)
+    other_char.set_board_position( (550, 500) )
+    sprites.add(other_char)
     
     ###############
     ## Game loop
