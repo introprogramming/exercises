510f2b6ac861be7572f843cca20b5df8200dd5d2
Johan Brook <johan.jvb@gmail.com>
2014-06-20 16:49:14 +0200

    Minor

--- exercises/spotify/searcher.py
+++ exercises/spotify/searcher.py
@@ -3,16 +3,19 @@ import sys
 
 if len(sys.argv) <= 1:
   print "You must provide something to search for!"
-  print "python searcher.py <song or artist>"
+  print "python searcher.py '<song or artist>'"
   sys.exit(0)
 
+q = sys.argv[1]
 
 sp = spotipy.Spotify()
-tracks = sp.search(q='Stairway To Heaven', limit=5)
+tracks = sp.search(q=q, limit=5)
 
-for i, t in enumerate(tracks['tracks']['items']):
+print "Searching for tracks of '{0}'\n".format(q)
+
+for i, t in enumerate(tracks['tracks']['items'], 1):
     name = t['name']
     album = t['album']['name']
     artists = ", ".join(map(lambda a: a['name'], t['artists']))
 
-    print "{0} - {1} ({2})".format(name, artists, album)
+    print "{3}. {0} - {1} ({2})".format(name, artists, album, i)
