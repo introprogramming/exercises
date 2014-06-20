b9951e58908cb5fbf533168ef73b6d148fe979ee
Johan Brook <johan.jvb@gmail.com>
2014-06-20 16:19:30 +0200

    Docs and tests

--- exercises/robbers_language/robber.py
+++ exercises/robbers_language/robber.py
@@ -20,20 +20,41 @@ CONSONANTS = "qwrtpsdfghjklzxcvbnm"
 SUFFIX = 'o'
 
 def is_consonant(char):
+  '''Returns true of char is consonant. False otherwise
+
+  >>> is_consonant('a')
+  False
+  >>> is_consonant('d')
+  True
+  '''
   return char.lower() in CONSONANTS.lower()
 
 def is_valid_file(input):
+  ''' Returns true if input is a valid text file.
+
+  >>> is_valid_file('./fixtures/test.txt')
+  True
+  >>> is_valid_file('./bogus')
+  False
+  '''
   # Educational moment: Learn how use google to find about
   # Python's built in helpers for validating a path for a file
   # and checking the file type.
   return os.path.isfile(input) and mimetypes.guess_type(input)[0] == "text/plain"
 
 def file_contents_from(path):
+  ''' Fetch file contents from a file at path.
+  Returns False if file at path cannot be read.
+
+  >>> file_contents_from('./fixtures/test.txt')
+  'En enkel fil.\\n'
+  >>> file_contents_from('./bogus')
+  False
+  '''
   try:
     f = open(path)
     return f.read()
   except IOError as e:
-    print "I/O error({0}): {1}".format(e.errno, e.strerror)
     return False
   else:
     f.close()
@@ -64,11 +85,16 @@ def add_suffix_if_consonant(input):
   return input+SUFFIX+input.lower() if is_consonant(input) else input
 
 def cli():
+  ''' Interactive CLI. Type 'exit' to quit.
+  '''
+  print 'Type "exit" or press Ctrl+C to leave.'
   input = _read_input()
 
   while input != "exit":
     print translate(input)
     input = _read_input()
+
+  print "Bye!"
 
 def _read_input():
   return raw_input("Enter Swedish text: ")
