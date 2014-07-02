5d664cd8028dcfc244d43c5c7ef199b89575e24b
Leif Schelin <scleif@student.chalmers.se>
2014-07-02 14:13:54 +0200

    Added functionality and implemented an OOP approach (that looks nicer)

--- exercises/midi/midi.py
+++ exercises/midi/midi.py
@@ -4,12 +4,21 @@
 
 import pygame
 from pygame import midi
+from pygame.locals import *
 import time, sys
 
+# Instruments at http://www.skoogmusic.com/manual/manual1.1/Skoog-Window/navigation/MIDI-Tab/index
+GRAND_PIANO = 0
+CHURCH_ORGAN = 19
+STEEL_GUITAR_AUCOUSTIC = 25
+
+instrument = GRAND_PIANO
+
+
 def init_keyboard(start_note):
+    """Creates a mapping from keyboard keys to music notes."""
     keyboard = {}
     
-    from pygame.locals import *
     keys = [K_TAB, K_1, K_q, K_2, K_w, K_3, K_e,
             K_r, K_5, K_t, K_6, K_y, K_u, K_8, K_i, K_9, K_o, K_0, K_p]
     
@@ -19,58 +28,68 @@ def init_keyboard(start_note):
         i = i+1
     return keyboard
 
-pygame.init()
-midi.init()
-
-print "Devices:", midi.get_count()
-out_device = midi.get_default_output_id()
-
-print "Uses:", out_device
-
-## Global variables
-output = midi.Output( out_device )
-keyboard = init_keyboard(53)
-
-# Instruments at http://www.skoogmusic.com/manual/manual1.1/Skoog-Window/navigation/MIDI-Tab/index
-GRAND_PIANO = 0
-CHURCH_ORGAN = 19
-STEEL_GUITAR_AUCOUSTIC = 25
-
-try:
-    output.set_instrument( STEEL_GUITAR_AUCOUSTIC )
-    
-    
-    screen = pygame.display.set_mode([250,80])
-    screen.fill((0,0,0))
-    pygame.display.set_caption("MIDI Keyboard")
-    pygame.display.flip()
-    
+def io_loop(keyboard, output, instrument=GRAND_PIANO):
+    """Main program loop. Handles IO."""
     octave = 0
     
     while 1:
-        e = pygame.event.wait()
-        if e.type == pygame.QUIT:
-            break
-        elif e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_ESCAPE:
+            e = pygame.event.wait()
+            
+            if e.type == pygame.QUIT:
                 break
-            elif e.key == pygame.K_z:
-                octave = octave - 1
-            elif e.key == pygame.K_x:
-                octave = octave + 1
-            else:
+            elif e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_ESCAPE:
+                    break
+                elif e.key == pygame.K_z:
+                    octave = octave - 1
+                elif e.key == pygame.K_x:
+                    octave = octave + 1
+                elif e.key == K_c:
+                    instrument = (instrument - 1)%128
+                    output.set_instrument( instrument )
+                elif e.key == K_v:
+                    instrument = (instrument + 1)%128
+                    output.set_instrument( instrument )
+                else:
+                    try:
+                        note = keyboard[e.key]
+                        output.note_on(note + 12*octave, 100)
+                    except KeyError:
+                        pass
+            elif e.type == pygame.KEYUP:
                 try:
                     note = keyboard[e.key]
-                    output.note_on(note + 12*octave, 100)
+                    output.note_off(note + 12*octave, 0)
                 except KeyError:
                     pass
-        elif e.type == pygame.KEYUP:
-            try:
-                note = keyboard[e.key]
-                output.note_off(note + 12*octave, 0)
-            except KeyError:
-                pass
+
+
+def main():
+    """Initiates program and loop."""
+    pygame.init()
+    midi.init()
+
+    print "Number of MIDI devices:", midi.get_count()
+    if midi.get_count() == 0:
+        print "No MIDI devices detected :P"
+        return
     
-finally:
-    del output
-    midi.quit()
+    out_device = midi.get_default_output_id()
+    print "Uses device no:", out_device
+    try:
+        output = midi.Output( out_device )
+        output.set_instrument( instrument )
+        keyboard = init_keyboard(53)
+        
+        screen = pygame.display.set_mode([250,80])
+        screen.fill((0,0,0))
+        pygame.display.set_caption("MIDI Keyboard")
+        pygame.display.flip()
+        
+        io_loop(keyboard, output, instrument)
+        
+    finally:
+        del output
+        midi.quit()
+
+main()
