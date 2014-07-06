e66078c061bfd474076e752e498e0e46db7accf2
Johan Brook <johan.jvb@gmail.com>
2014-07-06 21:55:34 +0200

    Docs

--- exercises/spotify/searcher.py
+++ exercises/spotify/searcher.py
@@ -1,8 +1,10 @@
 import spotipy
 import sys
 
+# Create new API object wrapper
 sp = spotipy.Spotify()
 
+# Format a track: "Track name – Artist, Album"
 def format_track(track):
   name = track['name'].encode('utf8')
   album = track['album']['name'].encode('utf8')
@@ -10,17 +12,22 @@ def format_track(track):
 
   return "{0} - {1} ({2})".format(name, artists, album)
 
+# Format an artist: "Artist name"
 def format_artist(artist):
   return artist['name'].encode('utf8')
 
+# Print API results with a formatter
 def show_results(results, formatter):
   for i, t in enumerate(results['items'], 1):
     print "{0}. {1}".format(i, formatter(t))
 
+# Search Spotify for a query with a limit
 def search(query, limit):
 
   tracks = sp.search(q=query, limit=limit)
   artists = sp.search(q=query, limit=limit, type='artist')
+
+  # Tracks
 
   print "\n"
   print "Found {1} tracks with: '{0}'\n".format(query, tracks['tracks']['total'])
@@ -29,6 +36,8 @@ def search(query, limit):
     print "(showing {0} first)".format(limit)
 
   show_results(tracks['tracks'], format_track)
+
+  # Artists
 
   print "\n"
   print "Found {1} artists with: '{0}'\n".format(query, artists['artists']['total'])
@@ -40,6 +49,9 @@ def search(query, limit):
 
   print "\n"
 
+# Usage:
+#
+#   python searcher.py <track or artist>
 if __name__ == "__main__":
   if len(sys.argv) <= 1:
     print "You must provide something to search for!"
