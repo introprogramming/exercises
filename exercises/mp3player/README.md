0821abf6174d62abe843789475dc55ee0ffdc497
Leif Schelin <scleif@student.chalmers.se>
2014-07-22 12:17:31 +0200

    Implemented pygame mp3-player

--- exercises/mp3player/README.md
+++ exercises/mp3player/README.md
@@ -1,15 +1,25 @@
 # Mp3-spelare
 
-## Svårighetsgrad: 1
+Ett program som spelar upp musik-filer som mp3 och wav.
+
+*Tips:*  
+Om musiken spelas upp men i fel hastighet kan man ange 'sample rate' mha `pygame.mixer.music.init(44100)`.
+
+- **Svårighetsgrad: 1**
 
 ## Delmoment
 
-1. Spela upp en .wav-fil.
-2. ...
+1. Importera pygame's musik-bibliotek, [pygame.mixer.music](http://www.pygame.org/docs/ref/music.html). Använd funktionen `load()` för att ladda in en musik-fil. **Svårighetsgrad 1**
+2. Vid `play()` så kommer den laddade filen att börja spelas upp. Programmet kommer dock att avslutas innan den hinner spela färdigt (eller ens börja spela). Därför behöver programmet vänta på input från användaren innan den får stängas av. Använd standard-funktionen `raw_input()` för detta. **Svårighetsgrad 1**
 
 ## Utbyggnad
-
-* ...
+* Låt användaren välja vilken fil som ska spelas upp genom att ange det som ett argument till programmet:  
+`python mitt_program.py "en fin sång.mp3"` **Svårighetsgrad 1**
+* Låt användaren kunna pausa musiken, spela upp en annan sång eller lägga en sång på kö, utan att avsluta programmet. Dessa finns som funktioner i `pygame.mixer.music`. **Svårighetsgrad 2**
 
 ## Externa bibliotek
+* [pygame](http://www.pygame.org/)
+
+alternativt
+
 * [pyglet](http://www.pyglet.org/download.html)
---
+++ exercises/mp3player/pygameMp3.py
@@ -0,0 +1,55 @@
+import pygame, sys
+#import time
+
+file = "sample.mp3"
+if len(sys.argv)>1:
+    file = sys.argv[1]
+
+pygame.mixer.init(44100) #Set sample rate
+pygame.mixer.music.load(file)
+pygame.mixer.music.play(0)
+
+def help():
+    print """\nCommands:
+\tend or exit \t Exit program
+\tr or rewind \t Play from the start
+\tp or pause \t Pause/unpause music
+\tq or queue \t Add music to queue
+\ts or stop \t Stop playing this one
+\tpl or play \t Play this file
+\th or help \t See this list again"""
+
+print """Welcome to this music player!
+
+You can give a file as an argument or use the commands below."""
+help()
+
+# Listen for user input
+s=raw_input()
+paused = False
+while s!="end" and s!="exit":
+    if s == "r" or s == "rewind":
+        pygame.mixer.music.rewind()
+    elif s == "p" or s == "pause":
+        if paused:
+            pygame.mixer.music.unpause()
+            paused = False
+        else:
+            pygame.mixer.music.pause()
+            paused = True
+    elif s == "q" or s == "queue":
+        file = raw_input("Add file to queue: ")
+        pygame.mixer.music.queue(file)
+    elif s == "s" or s == "stop":
+        pygame.mixer.music.stop()
+    elif s == "pl" or s == "play":
+        file = raw_input("Play this file instead: ")
+        pygame.mixer.music.load(file)
+        pygame.mixer.music.play(0)
+    elif s == "h" or s == "help":
+        help()
+    elif s == "":
+        pass
+    else:
+        print "I don't understand what you mean with '"+s+"'. Type `h` or `help` to see a list of commands."
+    s=raw_input()
