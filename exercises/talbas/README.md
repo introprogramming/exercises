1aa2e57e563f3d66c6c1f166285360310e006fd1
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-08-19 14:37:45 +0200

    talbaskonverterare mellan bin och dec

---
+++ exercises/talbas/README.md
@@ -0,0 +1,18 @@
+# Bin->dec & dec->bin converter
+
+Konvertera mellan talbaserna binär och decimal
+
+## Delmoment
+
+1. Skapa en funktion som, givet ett binärt tal i strängform, ger dess decimala motsvarighet. *Svårighetsgrad 1*
+2. Skapa en funktion som, givet ett decimalt tal i strängform, ger dess binära motsvarighet. *Svårighetsgrad 1*
+3. Parsa commandline-arguments *Svårighetsgrad 1*
+
+    python convert.py dec 5
+    101
+    python convert.py bin 101
+    5
+
+# Eventuella vidareutvecklingar
+
+- Lägga till talbaserna oktalt och hexadecimalt
---
+++ exercises/talbas/convert.py
@@ -0,0 +1,32 @@
+#
+# Decimal to binary (and back) converter 
+#
+# Usage:
+# python convert.py bin|dec 100
+#
+
+import sys
+
+def dectobin(dec_string):
+	"""Convert a decimal string to binary string"""
+	bin_string = bin(int(dec_string))
+	return bin_string[2:]
+
+def bintodec(bin_str):
+	"""Convert a binary string to decimal string"""
+	num = 0
+	index = 1
+	for c in reversed(bin_str):
+		num = num + index * int(c)
+		index = index * 2
+	return num
+
+if len(sys.argv) > 2:
+	if sys.argv[1] == 'bin':
+		print bintodec(sys.argv[2])
+	elif sys.argv[1] == 'dec':
+		print dectobin(sys.argv[2])
+	else:
+		print "Usage: convert.py bin|dec number"
+else:
+	print "Usage: convert.py bin|dec number"
