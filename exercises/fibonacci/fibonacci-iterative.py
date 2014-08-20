e67542618b6c1f603b4c6dc4fd66ef2c5c1a4c98
Behrouz Talebi <berrat2@gmail.com>
2014-08-20 12:23:51 +0200

    fixed the correct fibonacci series

--- exercises/fibonacci/fibonacci-iterative.py
+++ exercises/fibonacci/fibonacci-iterative.py
@@ -5,12 +5,19 @@ input = int(raw_input("Enter a number: "));
 def next_fibonacci(stop_after):
     """Iteratively searches for the fibonacci number that
     comes after the stop_after value"""
-    prev = 0;
+    if stop_after <= 0:
+         return 0;
+    if stop_after <= 2:
+	return 1;
+	
+    prev = 1;
     curr = 1;
+    count = 2;
     
-    while prev <= stop_after:
-        curr += prev;
+    while count <= stop_after:
+        curr = curr + prev;
         prev = curr - prev;
+        count = count + 1;
     
     return prev;
     
--- exercises/fibonacci/fibonacci-recursive.py
+++ exercises/fibonacci/fibonacci-recursive.py
@@ -4,16 +4,18 @@ input = int(raw_input("Enter a number: "));
 # The difference is that input() parameter is evaluated as a command,
 # whereas raw_input() evaluates the parameter as a string
 
-def next_fibonacci(prev, curr, stop_after):
+def next_fibonacci(stop_after):
     """Recursively searches for the fibonacci number that
     comes after the stop_after value"""
+    if stop_after < 2:
+        return stop_after;
+    
     
     # Stop if the sought fibonacci number has been found.
     # (The only reason I chose to look at the previous of the two numbers
     # is that I want it to include 0 as a possible return value as well,
     # which will happen with a negative input value).
-    if (prev > stop_after):
-        return prev;
-    return next_fibonacci(curr, curr + prev, stop_after)
+
+    return next_fibonacci(stop_after - 1) + next_fibonacci(stop_after - 2)
     
-print next_fibonacci(0, 1, input);
+print next_fibonacci(input);
