62812208c07167ae2a8d63be40e3fe582c13a8e1
Leif Schelin <schelinleif@gmail.com>
2014-08-20 12:42:22 +0200

    Improved recursive implementation

--- exercises/fibonacci/fibonacci-recursive.py
+++ exercises/fibonacci/fibonacci-recursive.py
@@ -16,6 +16,18 @@ def next_fibonacci(stop_after):
     # is that I want it to include 0 as a possible return value as well,
     # which will happen with a negative input value).
 
+    #It's this double-recursion that calculates the same value multiple times.
     return next_fibonacci(stop_after - 1) + next_fibonacci(stop_after - 2)
-    
-print next_fibonacci(input);
+
+
+def next_fibonacci_t(prev, curr, stop_after):
+    """Tail-recursive implementation with linear complexity."""
+    if (stop_after > 0):
+        return next_fibonacci_t(curr, curr + prev, stop_after-1)
+    elif (stop_after == 0):
+        return curr
+    else:
+        return 0
+
+print next_fibonacci_t(0, 1, input-1)
+#print next_fibonacci(input);
