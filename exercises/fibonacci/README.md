9e130edcec3fc9a6dba1aa9deb8df1010548ff78
phelerox <baxemyr@gmail.com>
2014-08-21 05:24:25 +0200

    La till en lite mer omfattande utbyggnad av fibonacci-uppgiften, inklusive en exempellösning.

--- exercises/fibonacci/README.md
+++ exercises/fibonacci/README.md
@@ -14,3 +14,4 @@ En applikation som tar ett tal som input, och skriver ut det första [fibonacci-
 - Låt användaren kunna ange input-talet som ett argument till programmet, exempelvis:  
 `python fibonacci.py 35`
 - Prova att generera talserien [rekursivt](http://interactivepython.org/courselib/static/pythonds/Recursion/recursionsimple.html) istället för iterativt. **Svårighetsgrad:** 2
+- Få programmet att fråga och svara användaren om och om igen istället för bara en gång. Spara dessutom den uträknade talserien i en lista, så att om användaren frågar om ett tal mindre än det största talet i den sparade talserien behöver talserien inte räknas ut på nytt. Istället letar man igenom listan efter svaret. Frågar däremot användaren om ett tal som är större än eller lika med det största talet i den sparade talserien så fortsätter man att generera talserien från slutet av den sparade talserien istället för att börja om på nytt. **Svårighetsgrad:** 2
---
+++ exercises/fibonacci/fibonacci-iterative-extended.py
@@ -0,0 +1,41 @@
+'''Iterative version that continously asks and answers the user,
+ and saves the calculated fibonacci series in case the user asks for something in that range.'''
+
+fibonacci = [0, 1]
+
+def main():
+    input = get_input()
+    while input != "exit":
+        try:
+            number = int(input)
+        except ValueError:
+            print "Please enter a valid integer!"
+            input = get_input()
+            continue
+        if number >= fibonacci[-1]:
+            print next_fibonacci(number)
+        else:
+            print find_next_fibonacci(number)
+        input = get_input()
+
+def get_input():
+    return raw_input("Enter a number or 'exit' to quit: ")
+
+def next_fibonacci(stop_after):
+    """Iteratively generates the fibonacci sequence, starting where the currently saved series ends, until the fibonacci number that
+    comes after the stop_after value is found"""
+    while fibonacci[-1] <= stop_after:
+        fibonacci.append(fibonacci[-2] + fibonacci[-1])
+
+    return fibonacci[-1]
+
+def find_next_fibonacci(stop_after):
+    """Naively searches the stored list for the fibonacci number that
+     comes after the stop_after value"""
+    #Binary search would be faster
+    for num in fibonacci:
+        if num > stop_after:
+            return num
+
+if __name__ == "__main__":
+    main()
