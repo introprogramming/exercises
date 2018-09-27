e6dcb03bbfcc1764e4fc694c0f79c2317e4669b7
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-27 22:23:54 +0200

    Reformat code

--- exercises/adventure/adventure.py
+++ exercises/adventure/adventure.py
@@ -1,10 +1,9 @@
-import sys
 from locations import *
-from location import *
 from switch import *
 
 current_location = 'haunted forest'
 here = locations[current_location]
+
 
 def move(direction):
     global here, current_location
@@ -14,9 +13,10 @@ def move(direction):
         current_location = new_loc
         here = locations[current_location]
     else:
-        print ("No route leading " + direction + ".\n")
+        print("No route leading " + direction + ".\n")
 
     look()
+
 
 def look(direction=""):
     if (direction):
@@ -25,17 +25,16 @@ def look(direction=""):
             view = locations[target].short_view()
         else:
             view = "There's nothing there."
-        print (view)
+        print(view)
     else:
-        print (here.name)
-        print (here.long_view())
-        print ("")
+        print(here.name)
+        print(here.long_view())
+        print("")
         here.print_exits()
-    
+
 
 def perform_action(input):
     here.do_something(input)
-
 
 
 def await_action():
@@ -43,14 +42,14 @@ def await_action():
 
     for case in switch(user_input[0]):
         if case('quit'):
-            print ("Bye!")
+            print("Bye!")
             return
 
-        if case('go','move'):
+        if case('go', 'move'):
             if len(user_input) > 1:
                 move(user_input[1])
             else:
-                print ("Where to?")
+                print("Where to?")
             break
 
         if case('look'):
@@ -62,8 +61,9 @@ def await_action():
 
         # We have a custom action
         perform_action(user_input)
-    
+
     await_action()
+
 
 """
   This starts the adventure game loop.
