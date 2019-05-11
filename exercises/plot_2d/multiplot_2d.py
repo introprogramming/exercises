e22fcf29be09b4fe8025ffc719337f28170c58ec
Vidar Magnusson <vidar.halmstad@hotmail.com>
2019-05-11 12:41:51 +0200

    Plot 2d now works on python 3.

--- exercises/plot_2d/multiplot_2d.py
+++ exercises/plot_2d/multiplot_2d.py
@@ -1,4 +1,3 @@
-
 import math, pygame, plot_2d
 from pygame.locals import *
 
@@ -6,12 +5,13 @@ from pygame.locals import *
 
 #############################################
 ## Standard colors (RGB)
-BLACK = ( 20,  20,  40)
+BLACK = (20, 20, 40)
 WHITE = (255, 255, 255)
-BLUE =  ( 20,  20, 255)
-GREEN = (  0, 255,   0)
-RED =   (255,   0,   0)
-CYAN  = ( 50, 255, 255)
+BLUE = (20, 20, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+CYAN = (50, 255, 255)
+
 
 #############################################
 ## Customize plot here
@@ -19,17 +19,17 @@ CYAN  = ( 50, 255, 255)
 def functions_to_print():
     """Write list of functions to plot here of the format [f_info1, f_info2, ...], where f_info == (f, color).
     Each function `f` must take a single number x and return a single number y."""
-    return [\
-            (lambda(x): -x*(x-3),            GREEN),\
-            (lambda(x): 2*math.cos(x)-0.5*x,  CYAN) \
-           ]
+    return [
+        (lambda x: -x * (x - 3), GREEN),
+        (lambda x: 2 * math.cos(x) - 0.5 * x, CYAN)]
 
-#Imports window settings from plot_2d.py
+
+# Imports window settings from plot_2d.py
 
 #############################################
 ## Let the program calculate the rest
 
-def plot_fun( f_info, X_MIN, X_MAX, N_POINTS, screen):
+def plot_fun(f_info, X_MIN, X_MAX, N_POINTS, screen):
     """Plots a function `f` with the specified settings.
     
     f_info == (f, color)"""
@@ -37,20 +37,20 @@ def plot_fun( f_info, X_MIN, X_MAX, N_POINTS, screen):
     pp = map(plot_2d.coordinate_to_position, cc)
     plot_2d.draw(screen, pp, f_info[1])
 
+
 def main():
     """Graphics: draws graphs on window and await EXIT or ESCAPE."""
     pygame.init()
     screen = pygame.display.set_mode([plot_2d.WIDTH, plot_2d.HEIGHT])
     pygame.display.set_caption('Multiplot 2d')
-    
+
     clock = pygame.time.Clock()
 
     screen.fill(plot_2d.background_color)
-    
-    map(lambda(f): plot_fun(f, plot_2d.X_MIN, plot_2d.X_MAX, plot_2d.N_POINTS, screen),\
-        functions_to_print())
+
+    map(lambda f: plot_fun(f, plot_2d.X_MIN, plot_2d.X_MAX, plot_2d.N_POINTS, screen), functions_to_print())
     plot_2d.draw_axis(screen)
-    
+
     pygame.display.update()
     while 1:
         e = pygame.event.wait()
@@ -58,6 +58,7 @@ def main():
             break
 
     pygame.quit()
+
 
 # if Python says run...
 if __name__ == '__main__':
--- exercises/plot_2d/plot_2d.py
+++ exercises/plot_2d/plot_2d.py
@@ -1,14 +1,14 @@
-
 import math, pygame
 from pygame.locals import *
 
 #############################################
 ## Standard colors (RGB)
-BLACK = ( 20,  20,  40)
+BLACK = (20, 20, 40)
 WHITE = (255, 255, 255)
-BLUE =  (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED =   (255,   0,   0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 
 #############################################
 ## Customize plot here
@@ -16,22 +16,23 @@ RED =   (255,   0,   0)
 def function_to_print(x):
     """Write function to plot here.
     Must take a single number x and return a single number y."""
-    return -x*(x-3)
+    return -x * (x - 3)
 
-#Range of window
+
+# Range of window
 X_MIN = 0.0
 X_MAX = 10.0
 Y_MIN = -10.0
 Y_MAX = 10.0
 
-#Tick interval on axes
+# Tick interval on axes
 X_TICK = 2.5
 Y_TICK = 2.5
 
-#Granularity of plotted functions, more points -> higher resolution plot
+# Granularity of plotted functions, more points -> higher resolution plot
 N_POINTS = 100
 
-#Colors
+# Colors
 background_color = BLACK
 plot_color = GREEN
 grid_color = WHITE
@@ -45,15 +46,17 @@ grid_color = WHITE
 WIDTH = 640
 HEIGHT = 480
 
-X_SIZE = X_MAX-X_MIN
-Y_SIZE = Y_MAX-Y_MIN
+X_SIZE = X_MAX - X_MIN
+Y_SIZE = Y_MAX - Y_MIN
+
 
 def coordinate_to_position(c):
     """Converts a model coordinate (vector) into a graphic position (pixel)"""
-    gx = (c[0]-X_MIN)*WIDTH/X_SIZE
-    gy = HEIGHT - (c[1]-Y_MIN)*HEIGHT/Y_SIZE
-    return [gx, gy]
-    
+    gx = (c[0] - X_MIN) * WIDTH / X_SIZE
+    gy = HEIGHT - (c[1] - Y_MIN) * HEIGHT / Y_SIZE
+    return gx, gy
+
+
 def curve_coordinates(f, x0, x1, points):
     """Returns list of coordinates
     
@@ -61,22 +64,25 @@ def curve_coordinates(f, x0, x1, points):
     Length of returned list == points."""
     coordinates = []
     x = x0
-    delta = (x1-x0)/(points-1)
+    delta = (x1 - x0) / (points - 1)
     while x <= x1:
         coordinates += [[x, f(x)]]
         x += delta
     return coordinates
 
+
 def linspace(x0, x1, points):
     """Returns a list of numbers of `points` elements, 
     with constant intervals between `x0` and `x1`"""
-    delta = (x1-x0)/(points-1)
-    return map(lambda(x): x0 + delta*x, range(points))
+    delta = (x1 - x0) / (points - 1)
+    return map(lambda x: x0 + delta * x, range(points))
+
 
 def curve_coordinates2(f, x0, x1, points):
     """(Alternative implementation):
     This is more compact and functional-like."""
-    return [ [x, f(x)] for x in linspace(x0,x1,points)]
+    return [[x, f(x)] for x in linspace(x0, x1, points)]
+
 
 def draw_ticks(screen, axis):
     """Draws appropriate ticks on the specified axis.
@@ -95,57 +101,56 @@ def draw_ticks(screen, axis):
         max = Y_MAX
         tick = Y_TICK
         limit = WIDTH
-    
+
     start = min + min % tick
     end = max - max % tick
-    points = (end-start)/tick + 1
-    t = limit/120
-    
+    points = (end - start) / tick + 1
+    t = limit / 120
+
     for x in linspace(start, end, int(points)):
-        c=[0,0]
+        c = [0, 0]
         c[axis] = x
         v = coordinate_to_position(c)
-        
-        a = v[1-axis]+t
-        if(a > limit):
+
+        a = v[1 - axis] + t
+        if a > limit:
             a = limit
-            
-        b = v[1-axis]-t
-        if(b < 0):
+
+        b = v[1 - axis] - t
+        if b < 0:
             b = 0
-            
-        #Copying v
-        s=list(v)
-        s[1-axis] = a
-        
-        e=list(v)
-        e[1-axis] = b
+
+        # Copying v
+        s = list(v)
+        s[1 - axis] = a
+
+        e = list(v)
+        e[1 - axis] = b
         pygame.draw.line(screen, grid_color, s, e, 2)
-    
+
+
 def draw_x_ticks(screen):
     """(Alternative implementation):
     Draws appropriate ticks on the X-axis."""
     start = X_MIN + X_MIN % X_TICK
     end = X_MAX - X_MAX % X_TICK
-    points = (end-start)/X_TICK + 1
-    
-    #t == half length of the tick line
-    t = HEIGHT/120
-    
-    #one iteration per tick
+    points = (end - start) / X_TICK + 1
+
+    # t == half length of the tick line
+    t = HEIGHT / 120
+
+    # one iteration per tick
     for x in linspace(start, end, int(points)):
         v = coordinate_to_position([x, 0])
-        a = v[1]+t
-        b = v[1]-t
-        
-        if(a > HEIGHT):
+        a = v[1] + t
+        b = v[1] - t
+
+        if a > HEIGHT:
             a = HEIGHT
-        if(b < 0):
+        if b < 0:
             b = 0
-        pygame.draw.line(screen, grid_color,\
-            [v[0], a],\
-            [v[0], b],\
-            2)
+        pygame.draw.line(screen, grid_color, [v[0], a], [v[0], b], 2)
+
 
 def draw_y_ticks(screen):
     """(Alternative implementation):
@@ -153,64 +158,65 @@ def draw_y_ticks(screen):
     This function mirrors draw_x_ticks(...)"""
     start = Y_MIN + Y_MIN % Y_TICK
     end = Y_MAX - Y_MAX % Y_TICK
-    points = (end-start)/Y_TICK + 1
-    
-    t = WIDTH/120
-    
+    points = (end - start) / Y_TICK + 1
+
+    t = WIDTH / 120
+
     for y in linspace(start, end, int(points)):
         v = coordinate_to_position([0, y])
-        #print v
-        
-        a = v[0]+t
-        b = v[0]-t
-        
-        if(a > WIDTH):
+        # print v
+
+        a = v[0] + t
+        b = v[0] - t
+
+        if (a > WIDTH):
             a = WIDTH
-        if(b < 0):
+        if (b < 0):
             b = 0
-        
-        pygame.draw.line(screen, grid_color,\
-            [a, v[1]],\
-            [b, v[1]],\
-            2)
+
+        pygame.draw.line(screen, grid_color, [a, v[1]], [b, v[1]], 2)
+
 
 def draw(screen, pp, plot_color):
     """Plots the points `pp` on the specified screen with the specified color."""
-    #Function
+    # Function
     pygame.draw.lines(screen, plot_color, False, pp, 3)
+
 
 def draw_axis(screen):
     """Draws the axes and ticks of the coordinate system."""
     ## Alternative implementations:
-    #draw_x_ticks(screen)
-    #draw_y_ticks(screen)
-    
+    # draw_x_ticks(screen)
+    # draw_y_ticks(screen)
+
     draw_ticks(screen, 0)
     draw_ticks(screen, 1)
-    
-    #X-Axis
-    pygame.draw.lines(screen, grid_color, False, map(coordinate_to_position,\
-        [[X_MIN, 0],[X_MAX, 0]]), 2)
-        
-    #Y-Axis
-    pygame.draw.lines(screen, grid_color, False, map(coordinate_to_position,\
-        [[0, Y_MIN],[0, Y_MAX]]), 2)
+
+    x_points = list(map(coordinate_to_position, [[X_MIN, 0], [X_MAX, 0]]))
+    y_points = list(map(coordinate_to_position, [[0, Y_MIN], [0, Y_MAX]]))
+
+    # X-Axis
+    pygame.draw.lines(screen, grid_color, False, x_points, 2)
+
+    # Y-Axis
+    pygame.draw.lines(screen, grid_color, False, y_points, 2)
+
 
 def main():
     """Graphics: draws graphs on window and await EXIT or ESCAPE."""
     pygame.init()
     screen = pygame.display.set_mode([WIDTH, HEIGHT])
     pygame.display.set_caption('Plot 2d')
-    
+
     clock = pygame.time.Clock()
 
     screen.fill(background_color)
-    
+
     cc = curve_coordinates(function_to_print, X_MIN, X_MAX, N_POINTS)
-    pp = map(coordinate_to_position, cc)
-    
-    #This would typically be done inside the loop, but since it is never
-    #updated: might as well keep it outside
+    pp = list(map(coordinate_to_position, cc))
+
+    # This would typically be done inside the loop, but since it is never
+    # updated: might as well keep it outside
     draw(screen, pp, plot_color)
     draw_axis(screen)
 
@@ -224,6 +230,7 @@ def main():
                 break
 
     pygame.quit()
+
 
 # if Python says run...
 if __name__ == '__main__':
